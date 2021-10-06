f865f4837041dac7f090e11de5e1f52555a7ff5c
jhasuraj01 <44930179+jhasuraj01@users.noreply.github.com>
2021-10-06 12:41:26 +0000

    Test

--- src/modules/deleteAllExcept.js
+++ src/modules/deleteAllExcept.js
@@ -4,6 +4,8 @@ const exec = require('@actions/exec');
 module.exports = async (except = []) => {
     const objects = await fs.readdir('./');
 
+    console.log(JSON.stringify(objects, null, 4))
+
     for (const object of objects) {
         if (except.includes(object) === false) {
            exec.exec(`rm ${object} -r`)
