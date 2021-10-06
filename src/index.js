3f344d8e3e0475f54fb86ad4726dad01979e439e
jhasuraj01 <44930179+jhasuraj01@users.noreply.github.com>
2021-10-06 10:58:03 +0000

    test

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 // import { Context } from "@actions/github/lib/context";
-import { GitHub } from "@actions/github";
+import { context } from "@actions/github";
 // import core from "@actions/core";
 // import { context } from "@actions/github/lib/utils";
 
 // Read the paths and remove trailing '/'
 // const outDir = core.getInput('outDir', { required: true }).replace(/\/+$/g, "")
 
-console.log(JSON.stringify(GitHub.context))
+console.log(JSON.stringify(context))
